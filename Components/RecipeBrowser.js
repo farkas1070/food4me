1b6afb45f5ae2c40a1188d9413f79956dc527bad
farkas1070 <71120991+farkas1070@users.noreply.github.com>
2022-09-16 18:22:32 +0200

    started pimping recipebrowser

--- Components/RecipeBrowser.js
+++ Components/RecipeBrowser.js
@@ -1,22 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+
+import React, { useContext, useState } from 'react'
+import { StyleSheet, Text, View, KeyboardAvoidingView, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'; 
+import { themeContext, foodContext } from "../Components/SetData.js"
 
 const RecipeBrowser = () => {
-  return (
-    <View style={styles.mainContainer}>
-      <Text>RecipeBrowser</Text>
-    </View>
-  )
+    const [darkTheme] = useContext(themeContext)
+    const [show, setShow] = useState(false)
+    const [foodarray, setFoodArray] = useContext(foodContext)
+    const [searchvalue, setSearchValue] = useState("")
+
+    return (
+        <View style={styles.mainContainer}>
+            
+                <Text style={{marginTop:30}}>Browse Recipes</Text>
+                <View style={{ flexDirection: 'row',alignItems: 'center',justifyContent: 'center'}}>
+                <Ionicons name="filter-outline" size={30} color="black" />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="New Username"
+                        placeholderTextColor="white"
+                        value={searchvalue}
+                        onChangeText={text => setSearchValue(text)}
+                    />
+                    <Ionicons name="search" size={30} color="black" />
+                </View>
+            
+        </View>
+    )
 }
 
 export default RecipeBrowser
 
 const styles = StyleSheet.create({
-    mainContainer:{
-        flex:1,
-        padding:20,
-        justifyContent: 'center',
+    mainContainer: {
+        flex: 1,
+        padding: 20,
         alignItems: 'center',
-        backgroundColor:"white"
+        backgroundColor: "white"
+    },
+    input:{
+        width:"60%",
+        height:40,
+        backgroundColor: "#fd5a43"
     }
+
 })
